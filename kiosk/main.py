fed5b1fbc49cd7403a2a397f043308a1d5665301
kansias <jjea88@gmail.com>
2023-09-01 12:01:38 +0900

    add cam, whisper, audio, serial, face-detection

--- kiosk/main.py
+++ kiosk/main.py
@@ -1,11 +1,22 @@
 
+from math import e
 import sys
+import time
 import cv2
 import numpy as np
+import pyaudio
+import wave
+import threading
+import openai
 from PySide2.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget
 from PySide2.QtGui import QImage, QPixmap
 from PySide2.QtCore import QTimer
-from ui import Ui_MainWindow
+from ui import QThread, Ui_MainWindow
+import serial
+import serial.tools.list_ports
+from konlpy.tag import Okt
+from openai.api_resources import file
+
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -23,9 +34,34 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
         # cam mirror
         # Check if video capture is successful
+        self.back_button.clicked.connect(self.backBtnClicked)
+        self.option_Button.clicked.connect(self.optionBtnClicked)
+        self.connect_button.clicked.connect(self.connectBtnClicked)
+        
+        # self.thread_start_recording = thread_start_recording()
+        # self.thread_transcribe_audio = thread_transcribe_audio()
+        
+        self.record = False
+        self.frames = []
+        self.p = pyaudio.PyAudio()
+        
         if not self.cap.isOpened():
             print("Failed to open webcam.")
             sys.exit()
+            
+        if self.get_available_arduino_ports():
+            self.comboBox.addItems(self.get_available_arduino_ports())
+        else:
+            self.comboBox.addItem("연결된 포트 없음")
+            
+        self.frames = []
+        openai.api_key = "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"
+        # Set the chunk size, sample format, channels, and rate
+        self.chunk = 1024
+        self.sample_format = pyaudio.paInt16
+        self.channels = 2
+        self.rate = 44100
+
         
         # Set a timer to fetch frames from the webcam
         self.timer = QTimer(self)
@@ -33,7 +69,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.timer.start(30)  # Fetch frame every 30ms
         
         # Load and add menu images to grid layout
-        self.menu_image_paths = ["./img/001.jpg", "./img/002.jpg"]
+        self.menu_image_paths = ["./img/001.jpg", "./img/002.jpg", "./img/002.jpg", "./img/002.jpg", "./img/002.jpg"]
         for index, img_path in enumerate(self.menu_image_paths):
             pixmap = QPixmap(img_path)
             
@@ -51,14 +87,15 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             label.setStyleSheet(u"QWidget {\n"
                                 "    \n"
                                 "    background-color: rgb(255, 255, 255);\n"
-                                "    border: 2px solid rgb(60, 60, 60);\n"
+                                "    border: 1px solid rgb(100, 100, 100);\n"
                                 "    border-radius: 3px;\n"
                                 "}")
             label.setFixedSize(pixmap.size())  # Set fixed size for the label
-            row = index // 2  
-            col = index % 2
+            row = index // 3  
+            col = index % 3
             
             self.gridLayout.addWidget(label, row, col)
+    
 
     def update_frame(self):
         ret, frame = self.cap.read()
@@ -69,15 +106,141 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             frame = cv2.flip(frame, 1)
             
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            faces = self.face_cascade.detectMultiScale(gray,  1.1 , 5) # detecMultiScale(image, scaleFactor, minNeighbors)
+            self.faces = self.face_cascade.detectMultiScale(gray,  1.1 , 1) # detecMultiScale(image, scaleFactor, minNeighbors)
             
-            for (x,y,w,h) in faces:
+            if len(self.faces) > 0:
+                self.record = True
+                # self.thread_start_recording.start()
+                # QTimer.singleShot(2000, self.make_audio_file)
+                # self.start_recording()
+            
+            for (x,y,w,h) in self.faces:
                 cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
                 
             height, width, channel = frame.shape
             step = channel * width
             qImg = QImage(frame.data, width, height, step, QImage.Format_RGB888)
             self.label.setPixmap(QPixmap.fromImage(qImg))
+            
+    def optionBtnClicked(self):
+        self.stackedWidget.setCurrentIndex(1)
+        
+    def backBtnClicked(self):
+        self.stackedWidget.setCurrentIndex(0)
+                
+    
+    def get_available_arduino_ports(self):
+        arduino_ports = []
+        available_ports = list(serial.tools.list_ports.comports())
+        for port in available_ports:
+            arduino_ports.append(port.device)
+        return arduino_ports
+    
+    def connectBtnClicked(self):
+        global arduino
+        arduino = serial.Serial(self.serial_port_combobox.currentText(), 9600)
+        arduino.flush()
+        # 연결되면 버튼 비활성화
+        self.connect_button.setEnabled(False)
+        self.combobox.setEnabled(False)
+        # 연결됨으로 커넥트 버튼 텍스트 변경
+        self.connect_button.setText("연결됨")
+        
+    def start_recording(self):
+        self.ment_label.setText("주문하실 메뉴를 말씀해주세요(인식중)")
+        self.ment_label.adjustSize()
+        stream = self.p.open(format=self.sample_format, channels=self.channels, rate=self.rate, frames_per_buffer=self.chunk, input=True)        
+        self.frames = []
+        while self.record:
+            data = stream.read(self.chunk)
+            self.frames.append(data)
+        stream.stop_stream()
+        stream.close()
+        
+        return self.frames
+    
+    def make_audio_file(self):
+        self.record = False
+        filename = "output.wav"
+        wf = wave.open(filename, 'wb')
+        wf.setnchannels(self.channels)
+        wf.setsampwidth(self.p.get_sample_size(self.sample_format))
+        wf.setframerate(self.rate)
+        wf.writeframes(b''.join(self.frames))
+        wf.close()
+        # self.thread_start_recording.stop()
+        # self.thread_transcribe_audio.start()
+        
+    
+    def transcribe_audio(self, filename):
+        self.ment_label.setText("인식중...")
+        with open(filename, 'rb') as f:
+            transcript = openai.Audio.transcribe("whisper-1", f)
+        result_text = transcript.text
+        self.ment_label.setText("주문 내역을 확인해주세요.")
+        self.ment_label.adjustSize()
+        self.parse_transcript(result_text)
+        
+        
+    def parse_transcript(self, transcript):
+        okt = Okt()
+        parsed_words = okt.nouns(transcript)
+        # 중복 단어 제거
+        parsed_words = list(set(parsed_words)) 
+        
+        menu_dict = {"마이쮸": "./img/001.jpg", "ABC 초콜릿": "./img/002.jpg", "./img/003.jpg": 3}
+        menu_order = []
+        
+        for word in parsed_words:
+            if "마이" in word and "쮸" in word:
+                menu_order.append("마이쮸")
+            elif word in menu_dict:
+                menu_order.append(word)
+            elif "ABC" in word and "초콜릿" in word:
+                menu_order.append("ABC 초콜릿")
+            elif "아이스" in word and "아메리카노" in word:
+                menu_order.append("아이스 아메리카노")
+        
+        # 주문 내역 출력
+        order_text = ""
+        for menu in menu_order:
+            order_text += menu + ", "
+        order_text = order_text[:-2]  # 마지막 ", " 제거
+        self.ment_label.setText(f"주문하신 메뉴는 '{order_text}'입니다. 주문을 진행하시겠습니까?")
+        self.ment_label.adjustSize()
+        
+        
+
+    def closeEvent(self, event) -> None:
+        self.cap.release()
+        self.p.terminate()
+        self.timer.stop()
+        
+        
+        return super().closeEvent(event)
+        
+
+# class thread_start_recording(QThread):
+#     def __init__(self):
+#         super(thread_start_recording, self).__init__()
+#         self.main = MainWindow()
+
+#     def run(self):
+#         self.main.start_recording()
+        
+#     def stop(self):
+#         self.main.record = False
+#         self.quit()
+# class thread_transcribe_audio(QThread):
+#     def __init__(self):
+#         super(thread_transcribe_audio, self).__init__()
+#         self.main = MainWindow()
+
+#     def run(self):
+#         self.main.transcribe_audio("output.wav")
+        
+#     def stop(self):
+#         self.quit()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- kiosk/ui.py
+++ kiosk/ui.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'designerPQbpFw.ui'
+## Form generated from reading UI file 'kioskcXFLjr.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
 ##
@@ -17,74 +17,398 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(1072, 600)
+        MainWindow.resize(1334, 707)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.centralwidget.setStyleSheet(u"QWidget {\n"
-"	\n"
-"	background-color: rgb(255, 255, 255);\n"
+        self.centralwidget.setStyleSheet(u"QWidget  {\n"
+"background-color: #222222;\n"
 "}")
-        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout_8 = QVBoxLayout(self.centralwidget)
+        self.verticalLayout_8.setObjectName(u"verticalLayout_8")
+        self.stackedWidget = QStackedWidget(self.centralwidget)
+        self.stackedWidget.setObjectName(u"stackedWidget")
+        self.page = QWidget()
+        self.page.setObjectName(u"page")
+        self.verticalLayout = QVBoxLayout(self.page)
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.horizontalLayout = QHBoxLayout()
+        self.top_frame = QFrame(self.page)
+        self.top_frame.setObjectName(u"top_frame")
+        self.top_frame.setMinimumSize(QSize(0, 60))
+        self.top_frame.setMaximumSize(QSize(16777215, 55))
+        self.top_frame.setStyleSheet(u"QFrame {\n"
+"background-color: #fafafa;\n"
+"border-radius : 5px;\n"
+"\n"
+"}")
+        self.top_frame.setFrameShape(QFrame.StyledPanel)
+        self.top_frame.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_4 = QHBoxLayout(self.top_frame)
+        self.horizontalLayout_4.setSpacing(22)
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.horizontalLayout_4.setContentsMargins(0, -1, 0, 11)
+        self.option_Button = QPushButton(self.top_frame)
+        self.option_Button.setObjectName(u"option_Button")
+        self.option_Button.setMinimumSize(QSize(30, 30))
+        self.option_Button.setMaximumSize(QSize(30, 30))
+        self.option_Button.setStyleSheet(u"QPushButton {\n"
+"background-color: #fafafa;\n"
+"}")
+        self.option_Button.setFlat(True)
+
+        self.horizontalLayout_4.addWidget(self.option_Button)
+
+        self.top_label1 = QLabel(self.top_frame)
+        self.top_label1.setObjectName(u"top_label1")
+        self.top_label1.setMinimumSize(QSize(0, 0))
+        font = QFont()
+        font.setFamily(u"\ud734\uba3c\ub465\uadfc\ud5e4\ub4dc\ub77c\uc778")
+        font.setPointSize(16)
+        self.top_label1.setFont(font)
+        self.top_label1.setLayoutDirection(Qt.LeftToRight)
+        self.top_label1.setStyleSheet(u"")
+        self.top_label1.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+
+        self.horizontalLayout_4.addWidget(self.top_label1)
+
+        self.top_label2 = QLabel(self.top_frame)
+        self.top_label2.setObjectName(u"top_label2")
+        self.top_label2.setMinimumSize(QSize(0, 35))
+        font1 = QFont()
+        font1.setFamily(u"\ud734\uba3c\ub465\uadfc\ud5e4\ub4dc\ub77c\uc778")
+        font1.setPointSize(22)
+        self.top_label2.setFont(font1)
+        self.top_label2.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
+
+        self.horizontalLayout_4.addWidget(self.top_label2)
+
+
+        self.verticalLayout.addWidget(self.top_frame)
+
+        self.main_frame = QFrame(self.page)
+        self.main_frame.setObjectName(u"main_frame")
+        self.horizontalLayout = QHBoxLayout(self.main_frame)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.Menu_layout = QVBoxLayout()
+        self.menu_frame = QFrame(self.main_frame)
+        self.menu_frame.setObjectName(u"menu_frame")
+        self.menu_frame.setMinimumSize(QSize(900, 0))
+        self.menu_frame.setFrameShape(QFrame.Panel)
+        self.menu_frame.setFrameShadow(QFrame.Raised)
+        self.Menu_layout = QVBoxLayout(self.menu_frame)
         self.Menu_layout.setObjectName(u"Menu_layout")
-        self.gridLayout = QGridLayout()
-        self.gridLayout.setObjectName(u"gridLayout")
-        self.label_3 = QLabel(self.centralwidget)
+        self.Menu_layout.setContentsMargins(5, -1, 5, 5)
+        self.frame_2 = QFrame(self.menu_frame)
+        self.frame_2.setObjectName(u"frame_2")
+        self.frame_2.setMaximumSize(QSize(16777215, 50))
+        self.frame_2.setStyleSheet(u"")
+        self.frame_2.setFrameShape(QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_2 = QHBoxLayout(self.frame_2)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.label_3 = QLabel(self.frame_2)
         self.label_3.setObjectName(u"label_3")
-        self.label_3.setMinimumSize(QSize(640, 0))
-        self.label_3.setMaximumSize(QSize(1100, 15))
+        self.label_3.setMinimumSize(QSize(640, 30))
+        self.label_3.setMaximumSize(QSize(640, 30))
+        font2 = QFont()
+        font2.setFamily(u"\ud734\uba3c\ub465\uadfc\ud5e4\ub4dc\ub77c\uc778")
+        font2.setPointSize(14)
+        self.label_3.setFont(font2)
+        self.label_3.setStyleSheet(u"QLabel {\n"
+"background-color: #fafafa;\n"
+"border-radius : 5px;\n"
+"}")
         self.label_3.setAlignment(Qt.AlignCenter)
 
-        self.Menu_layout.addWidget(self.label_3)
-        self.Menu_layout.addLayout(self.gridLayout)
+        self.horizontalLayout_2.addWidget(self.label_3)
 
 
-        self.horizontalLayout.addLayout(self.Menu_layout)
+        self.Menu_layout.addWidget(self.frame_2)
 
-        self.Order_layout = QVBoxLayout()
+        self.list_frame = QFrame(self.menu_frame)
+        self.list_frame.setObjectName(u"list_frame")
+        self.list_frame.setStyleSheet(u"QFrame {\n"
+"background-color: #fafafa;\n"
+"border-radius : 5px;\n"
+"\n"
+"}")
+        self.list_frame.setFrameShape(QFrame.StyledPanel)
+        self.list_frame.setFrameShadow(QFrame.Plain)
+        self.verticalLayout_2 = QVBoxLayout(self.list_frame)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.gridLayout = QGridLayout()
+        self.gridLayout.setObjectName(u"gridLayout")
+
+        self.verticalLayout_2.addLayout(self.gridLayout)
+
+
+        self.Menu_layout.addWidget(self.list_frame)
+
+
+        self.horizontalLayout.addWidget(self.menu_frame)
+
+        self.order_frame = QFrame(self.main_frame)
+        self.order_frame.setObjectName(u"order_frame")
+        self.order_frame.setMaximumSize(QSize(455, 16777215))
+        self.order_frame.setLayoutDirection(Qt.LeftToRight)
+        self.order_frame.setAutoFillBackground(False)
+        self.order_frame.setFrameShape(QFrame.Panel)
+        self.order_frame.setFrameShadow(QFrame.Raised)
+        self.order_frame.setLineWidth(1)
+        self.Order_layout = QVBoxLayout(self.order_frame)
         self.Order_layout.setObjectName(u"Order_layout")
-        self.Order_layout.SetMinimumSize(QSize(400, 0))
-        self.widget = QWidget(self.centralwidget)
-        self.widget.setObjectName(u"widget")
-        self.widget.setMinimumSize(QSize(550, 480))
-        self.verticalLayout_3 = QVBoxLayout(self.widget)
+        self.Order_layout.setContentsMargins(-1, 0, -1, -1)
+        self.label_4 = QLabel(self.order_frame)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setMinimumSize(QSize(0, 30))
+        self.label_4.setMaximumSize(QSize(16777215, 30))
+        self.label_4.setFont(font2)
+        self.label_4.setStyleSheet(u"QLabel {\n"
+"background-color: #fafafa;\n"
+"border-radius : 5px;\n"
+"}")
+        self.label_4.setAlignment(Qt.AlignCenter)
+
+        self.Order_layout.addWidget(self.label_4)
+
+        self.cam_frame = QFrame(self.order_frame)
+        self.cam_frame.setObjectName(u"cam_frame")
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.cam_frame.sizePolicy().hasHeightForWidth())
+        self.cam_frame.setSizePolicy(sizePolicy)
+        self.cam_frame.setMinimumSize(QSize(320, 240))
+        self.cam_frame.setMaximumSize(QSize(2000, 240))
+        self.cam_frame.setFrameShape(QFrame.Box)
+        self.cam_frame.setFrameShadow(QFrame.Sunken)
+        self.cam_frame.setLineWidth(4)
+        self.verticalLayout_3 = QVBoxLayout(self.cam_frame)
         self.verticalLayout_3.setObjectName(u"verticalLayout_3")
-        self.label = QLabel(self.widget)
-        self.centralwidget.layout().setAlignment(self.label, Qt.AlignCenter)
+        self.label = QLabel(self.cam_frame)
         self.label.setObjectName(u"label")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy1)
+        self.label.setMaximumSize(QSize(320, 16777215))
+        self.label.setStyleSheet(u"QLabel {\n"
+"	background-color: rgb(218, 218, 218);\n"
+"}")
+        self.label.setFrameShape(QFrame.NoFrame)
+        self.label.setFrameShadow(QFrame.Plain)
+        self.label.setLineWidth(1)
         self.label.setAlignment(Qt.AlignHCenter|Qt.AlignTop)
 
         self.verticalLayout_3.addWidget(self.label)
 
 
-        self.Order_layout.addWidget(self.widget)
+        self.Order_layout.addWidget(self.cam_frame, 0, Qt.AlignHCenter)
 
-        self.label_2 = QLabel(self.centralwidget)
+        self.ment_frame = QFrame(self.order_frame)
+        self.ment_frame.setObjectName(u"ment_frame")
+        self.ment_frame.setFrameShape(QFrame.StyledPanel)
+        self.ment_frame.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_4 = QVBoxLayout(self.ment_frame)
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.ment_label = QLabel(self.ment_frame)
+        self.ment_label.setObjectName(u"ment_label")
+        self.ment_label.setMinimumSize(QSize(0, 50))
+        self.ment_label.setMaximumSize(QSize(16777215, 50))
+        font3 = QFont()
+        font3.setFamily(u"\ud568\ucd08\ub86c\ub3cb\uc6c0")
+        font3.setPointSize(16)
+        font3.setBold(False)
+        font3.setWeight(50)
+        self.ment_label.setFont(font3)
+        self.ment_label.setStyleSheet(u"QLabel {\n"
+"	color: rgb(255, 182, 11);\n"
+"}")
+        self.ment_label.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout_4.addWidget(self.ment_label)
+
+        self.ex_label = QLabel(self.ment_frame)
+        self.ex_label.setObjectName(u"ex_label")
+        self.ex_label.setMinimumSize(QSize(0, 50))
+        self.ex_label.setMaximumSize(QSize(16777215, 50))
+        font4 = QFont()
+        font4.setFamily(u"\ud734\uba3c\ub465\uadfc\ud5e4\ub4dc\ub77c\uc778")
+        font4.setPointSize(12)
+        self.ex_label.setFont(font4)
+        self.ex_label.setStyleSheet(u"QLabel {\n"
+"	\n"
+"	color: rgb(245, 245, 245);\n"
+"}")
+        self.ex_label.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout_4.addWidget(self.ex_label)
+
+        self.frame = QFrame(self.ment_frame)
+        self.frame.setObjectName(u"frame")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_5 = QVBoxLayout(self.frame)
+        self.verticalLayout_5.setObjectName(u"verticalLayout_5")
+        self.label_2 = QLabel(self.frame)
         self.label_2.setObjectName(u"label_2")
+        self.label_2.setMinimumSize(QSize(0, 30))
+        self.label_2.setMaximumSize(QSize(16777215, 30))
+        self.label_2.setFont(font2)
+        self.label_2.setStyleSheet(u"QLabel {\n"
+"background-color: #fafafa;\n"
+"border-radius : 5px;\n"
+"}")
         self.label_2.setAlignment(Qt.AlignCenter)
 
-        self.Order_layout.addWidget(self.label_2)
+        self.verticalLayout_5.addWidget(self.label_2)
+
+        self.frame_3 = QFrame(self.frame)
+        self.frame_3.setObjectName(u"frame_3")
+        self.frame_3.setStyleSheet(u"QFrame {\n"
+"	background-color: #fafafa;\n"
+"}")
+        self.frame_3.setFrameShape(QFrame.StyledPanel)
+        self.frame_3.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_6 = QVBoxLayout(self.frame_3)
+        self.verticalLayout_6.setObjectName(u"verticalLayout_6")
+        self.gridLayout_2 = QGridLayout()
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+
+        self.verticalLayout_6.addLayout(self.gridLayout_2)
 
 
-        self.horizontalLayout.addLayout(self.Order_layout)
+        self.verticalLayout_5.addWidget(self.frame_3)
 
 
-        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.verticalLayout_4.addWidget(self.frame)
+
+
+        self.Order_layout.addWidget(self.ment_frame)
+
+
+        self.horizontalLayout.addWidget(self.order_frame)
+
+
+        self.verticalLayout.addWidget(self.main_frame)
+
+        self.stackedWidget.addWidget(self.page)
+        self.page_2 = QWidget()
+        self.page_2.setObjectName(u"page_2")
+        self.verticalLayout_9 = QVBoxLayout(self.page_2)
+        self.verticalLayout_9.setObjectName(u"verticalLayout_9")
+        self.frame_5 = QFrame(self.page_2)
+        self.frame_5.setObjectName(u"frame_5")
+        font5 = QFont()
+        font5.setFamily(u"\uc0c8\uad74\ub9bc")
+        self.frame_5.setFont(font5)
+        self.frame_5.setStyleSheet(u"QFrame {\n"
+"	background-color: #f9f9f9;\n"
+"	\n"
+"\n"
+"}")
+        self.frame_5.setFrameShape(QFrame.StyledPanel)
+        self.frame_5.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_7 = QVBoxLayout(self.frame_5)
+        self.verticalLayout_7.setObjectName(u"verticalLayout_7")
+        self.frame_4 = QFrame(self.frame_5)
+        self.frame_4.setObjectName(u"frame_4")
+        self.frame_4.setMinimumSize(QSize(600, 0))
+        self.frame_4.setMaximumSize(QSize(600, 16777215))
+        self.frame_4.setAutoFillBackground(False)
+        self.frame_4.setFrameShape(QFrame.StyledPanel)
+        self.frame_4.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_6 = QHBoxLayout(self.frame_4)
+        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
+        self.label_5 = QLabel(self.frame_4)
+        self.label_5.setObjectName(u"label_5")
+        self.label_5.setFont(font5)
+
+        self.horizontalLayout_6.addWidget(self.label_5, 0, Qt.AlignRight)
+
+        self.comboBox = QComboBox(self.frame_4)
+        self.comboBox.setObjectName(u"comboBox")
+        self.comboBox.setMinimumSize(QSize(200, 0))
+        self.comboBox.setMaximumSize(QSize(200, 16777215))
+        self.comboBox.setStyleSheet(u"QComboBox {\n"
+"	\n"
+"	background-color: #f9f9f9;\n"
+"\n"
+"}")
+
+        self.horizontalLayout_6.addWidget(self.comboBox, 0, Qt.AlignLeft)
+
+        self.connect_button = QPushButton(self.frame_4)
+        self.connect_button.setObjectName(u"connect_button")
+        self.connect_button.setMinimumSize(QSize(0, 30))
+        self.connect_button.setMaximumSize(QSize(150, 30))
+        self.connect_button.setStyleSheet(u"QPushButton {\n"
+"  height : 30px;\n"
+"  font-size: 15px;\n"
+"  text-align: center;\n"
+"  text-decoration: none;\n"
+"  outline: none;\n"
+"  color: #fff;\n"
+"  background-color: rgb(17, 229, 2);\n"
+"  border: none;\n"
+"  border-radius: 15px;\n"
+"  font-weight : 900;\n"
+"}")
+
+        self.horizontalLayout_6.addWidget(self.connect_button)
+
+
+        self.verticalLayout_7.addWidget(self.frame_4, 0, Qt.AlignHCenter|Qt.AlignTop)
+
+        self.back_button = QPushButton(self.frame_5)
+        self.back_button.setObjectName(u"back_button")
+        self.back_button.setMinimumSize(QSize(550, 50))
+        self.back_button.setMaximumSize(QSize(550, 50))
+        self.back_button.setStyleSheet(u"QPushButton {\n"
+"  height : 30px;\n"
+"  font-size: 15px;\n"
+"  text-align: center;\n"
+"  text-decoration: none;\n"
+"  outline: none;\n"
+"  color: #fff;\n"
+"  background-color: #222222;\n"
+"  border: none;\n"
+"  border-radius: 15px;\n"
+"  font-weight : 900;\n"
+"}")
+
+        self.verticalLayout_7.addWidget(self.back_button, 0, Qt.AlignHCenter)
+
+
+        self.verticalLayout_9.addWidget(self.frame_5)
+
+        self.stackedWidget.addWidget(self.page_2)
+
+        self.verticalLayout_8.addWidget(self.stackedWidget)
 
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
 
+        self.stackedWidget.setCurrentIndex(0)
+        self.comboBox.setCurrentIndex(-1)
+
+
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"오키", None))
-        self.label_3.setText(QCoreApplication.translate("MainWindow", u"메뉴", None))
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.option_Button.setText("")
+        self.top_label1.setText(QCoreApplication.translate("MainWindow", u"Smart Kiosk", None))
+        self.top_label2.setText(QCoreApplication.translate("MainWindow", u"\uc624\ud0a4", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"\uba54\ub274", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"\uc8fc\ubb38 \uc811\uc218", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"CAM", None))
-        self.label_2.setText(QCoreApplication.translate("MainWindow", u"주문하실 메뉴를 말씀해주세요.", None))
+        self.ment_label.setText(QCoreApplication.translate("MainWindow", u"\uc8fc\ubb38\ud558\uc2e4 \uba54\ub274\ub97c \ub9d0\uc500\ud574\uc8fc\uc138\uc694.", None))
+        self.ex_label.setText(QCoreApplication.translate("MainWindow", u"\uc608) \"000 \uc8fc\uc138\uc694\"", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"\uc8fc\ubb38 \ub0b4\uc5ed", None))
+        self.label_5.setText(QCoreApplication.translate("MainWindow", u"\uc7a5\uce58", None))
+        self.connect_button.setText(QCoreApplication.translate("MainWindow", u"Connect", None))
+        self.back_button.setText(QCoreApplication.translate("MainWindow", u"Back", None))
     # retranslateUi
 
